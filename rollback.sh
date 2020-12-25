bcc1979a3b4639fa33760bf5ecbb724cd0063863
Som <som@Soms-MacBook-Pro.local>
2020-12-24 19:20:49 -0500

    .

--- rollback.sh
+++ rollback.sh
@@ -2,13 +2,12 @@
 git pull origin master
 k delete -f ./base/rollout.yml
 rm ./base/rollout.yml
-sed -i '' -e '$ d' base/kustomization.yml 
-sed -i -e '$a- service.yml' ./base/kustomization.yml
+mv ./base/kustomization.yml.orig ./base/kustomization.yml
 git status
 git add --all
 git commit -m "."
 git push 
-k argo rollouts get rollout simple-deployment -n dev-som
+kubectl argo rollouts get rollout simple-deployment -n dev-som
 echo "test"
 kustomize build overlays
 
